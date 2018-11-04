ba8c6a3ad858ef7f9ad98b013ad3d0fe1e35848e
Juno Morrow <sabotaione@gmail.com>
2018-11-04 16:03:18 -0500

    added game

---
+++ Mrooowr/Assets/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public bool selected = true;
+	public float speed = 2f;
+	public bool facingRight = true;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selected){
+			rb.velocity = Vector2.zero;
+			if (Input.GetKey(KeyCode.D)) { 
+				rb.velocity += new Vector2(1.0f, 0f) * speed;
+				if (!facingRight) {
+					transform.localScale *= new Vector2(-1f, 1f);
+					facingRight = true;
+				}
+			}
+
+			if (Input.GetKey(KeyCode.A)) { 
+				rb.velocity += new Vector2(-1.0f, 0f) * speed;
+				if (facingRight){
+					transform.localScale *= new Vector2(-1f, 1f);
+					facingRight = false;
+				} 
+			}
+			if (Input.GetKey(KeyCode.W)) { 
+				rb.velocity += new Vector2(0f, 1f) * speed;
+			}
+
+			if (Input.GetKey(KeyCode.S)) { 
+				rb.velocity += new Vector2(0f,-1f) * speed;
+			}
+		
+		} else {
+
+			rb.velocity = Vector2.zero;
+		}
+	}
+}
---
+++ Mrooowr/Assets/Selection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour {
+
+	public int player = 1;
+	int selected = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player == 1 && Input.GetButtonDown("P1_Next")){
+			if (selected < transform.childCount - 1) selected++; else selected = 00;
+			for(int i = 0; i < transform.childCount; i++){
+				transform.GetChild(i).gameObject.GetComponent<Move>().selected = false;
+			}
+			transform.GetChild(selected).gameObject.GetComponent<Move>().selected = true;
+		}
+	}
+}
