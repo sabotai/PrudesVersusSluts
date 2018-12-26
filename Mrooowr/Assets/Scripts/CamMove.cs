54a3380e7ee5b7774fb33b17b5f733e6fa1db241
Juno AM <sabotaione@gmail.com>
2018-12-25 23:53:42 -0500

    rainbow road! small tweak to parallax

--- Mrooowr/Assets/Scripts/CamMove.cs
+++ Mrooowr/Assets/Scripts/CamMove.cs
@@ -9,19 +9,23 @@ public class CamMove : MonoBehaviour {
 	public bool max = false;
 	float maxRight, maxLeft;
 	public float speed = 0.6f;
-	public Transform reverseSlave;
+	GameObject[] reversed;
 	public float parallaxAmt = 0.5f;
 	// Use this for initialization
 	void Start () {	
 		Manager.howManyChars = slutParent.childCount + prudeParent.childCount;
 		maxRight = GameObject.Find("RightBound").transform.position.x;
 		maxLeft = GameObject.Find("LeftBound").transform.position.x;
+		reversed = GameObject.FindGameObjectsWithTag("Foreground");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 move = Vector3.Lerp(transform.position, AvgCharPos(), speed);
-		if (reverseSlave) reverseSlave.position += parallaxAmt * (transform.parent.position - move);
+		if (reversed.Length > 0) {
+			foreach(GameObject fore in reversed)
+				fore.transform.position += parallaxAmt * (transform.parent.position - move);
+		}
 		transform.parent.position = move;//Vector3.Lerp(transform.position, AvgCharPos(), speed);
 
 
---
+++ Mrooowr/Assets/Scripts/Pan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan : MonoBehaviour {
+	public float speed = 1f;
+	public Vector3 direction;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += direction * speed * Time.deltaTime;
+	}
+}
