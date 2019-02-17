2255d3cc181537c48d818e6a58a3e4d465c18157
sabotai <sabotaione@gmail.com>
2019-02-16 22:22:42 -0500

    changed music ... updated ui and changed a few small things

---
+++ Mrooowr/Assets/Scripts/MusicCredit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicCredit : MonoBehaviour {
+	public string pretext = "";
+	public string posttext = "";
+	// Use this for initialization
+	void Start () {
+						GetComponent<Text>().text = pretext + GameObject.FindWithTag("Music").GetComponent<AudioSource>().clip.name + posttext;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 1; i < 3; i++){
+			//if (GetComponent<Text>().text != "" && Input.GetAxis("P" + i + "_Horizontal") != 0f){
+			if (Input.GetAxis("P" + i + "_Horizontal") != 0f){
+
+				GetComponent<Text>().text = pretext + GameObject.FindWithTag("Music").GetComponent<AudioSource>().clip.name + posttext;
+			}
+
+		}
+	}
+}
